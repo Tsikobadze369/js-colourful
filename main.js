eba78cc019b838d10c5d839d2a541165e16089ba
Tinatin <tinatinatsikobadze@gmail.com>
2022-10-29 20:29:23 +0400

    click

---
+++ main.js
@@ -0,0 +1,4 @@
+let button = document.querySelector("button");
+button.addEventListener("click", function Click(){
+   document.body.style.backgroundColor =  'rgb(' + Math.round(Math.random() * 500) + ',' + Math.round(Math.random() * 590) + ',' + Math.round(Math.random() * 355) + ')';
+})
